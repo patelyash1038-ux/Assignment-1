fcd1d7528754dd69ba7b7e0a37d5b49a5d4e791d
patelyash1038-ux <patelyash1038@gmail.com>
2026-09-17 09:59:43 +0530

    28.c

--- 28.c
+++ 28.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#include<math.h>
+//#include<math.h>
 int main(){
-    int NUM,num,n=0,final=0,remainder;
+    int NUM,num,n=0,final=0,remainder,power;
     printf("Enter one no.= ");
     scanf("%d",&num);
     NUM=num;
@@ -9,11 +9,24 @@ int main(){
         NUM/=10;
         ++n;
     }
+    /*
     NUM=num;
     while (NUM!=0)
     {
         remainder=NUM%10;
         final+=round(pow(remainder,n));
+        NUM/=10;
+    }
+        
+    */
+    NUM=num;
+    while(NUM!=0){
+        power=1;
+        remainder=NUM%10;
+        for(int i=0;i<n;i++){
+            power*=remainder;
+        }
+        final+=power;
         NUM/=10;
     }
     if(final == num){
