4b7fc2128e724755886da7eeb44f22d7f4b1d359
patelyash1038-ux <patelyash1038@gmail.com>
2026-09-17 10:00:38 +0530

    29.c

--- 29.c
+++ 29.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<math.h>
+//#include<math.h>
 int main(){
     int num,remainder,NUM,final=0,n=0;
     printf("Enter one no.= ");
@@ -10,12 +10,19 @@ int main(){
         ++n;
     }
     NUM=num;
+    /*
     while(NUM!=0){
         remainder=NUM%10;
         final+=round((remainder)*(pow(10,n-1))); // OR final*10 + remainder
         n--;
         NUM/=10;
     }
+        */
+       while (NUM!=0){
+        remainder=NUM%10;
+        final=(10*final)+remainder;
+        NUM/=10;
+       }
     if(final==num){
         printf("%d is palindrome",num);
     }
